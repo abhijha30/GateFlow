151feb25edc993e27588d470102aba100147a7cf
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-03-31 11:54:09 +0530

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -3,11 +3,11 @@ import streamlit as st
 def admin_login():
     st.subheader("🔐 Admin Login")
 
-    username = st.text_input("i_avinashjha")
-    password = st.text_input("Password", type="Avinash@123")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password") 
 
     if st.button("Login"):
-        if username == "admin" and password == "gateflow123":
+        if username == "i_avinashjha" and password == "gateflow123":
             st.session_state["admin"] = True
         else:
             st.error("Invalid credentials")
