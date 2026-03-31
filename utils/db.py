014838639666c538462b2d244da10c63ee357051
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-03-31 20:32:05 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_URL = os.getenv("https://ahaiguauzayaslaazokj.supabase.co")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
