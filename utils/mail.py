3185dfeb0d7c267a09ed3e76b7042333969c57bb
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-04-03 17:23:16 +0530

    Update mail.py

--- utils/mail.py
+++ utils/mail.py
@@ -1,17 +1,25 @@
-import smtplib, os
+import smtplib
 from email.message import EmailMessage
 
-def send_qr(receiver, file):
-    msg = EmailMessage()
-    msg["Subject"] = "Your GateFlow Pass"
-    msg["From"] = os.getenv("EMAIL")
-    msg["To"] = receiver
+def send_qr(email, file_path):
+    try:
+        msg = EmailMessage()
+        msg['Subject'] = "GateFlow QR Pass"
+        msg['From'] = "your_email@gmail.com"
+        msg['To'] = email
 
-    msg.set_content("Your QR pass is attached.")
+        msg.set_content("Your QR Pass is attached")
 
-    with open(file, "rb") as f:
-        msg.add_attachment(f.read(), maintype="image", subtype="png", filename="qr.png")
+        with open(file_path, 'rb') as f:
+            msg.add_attachment(f.read(), maintype='image', subtype='png', filename="qr.png")
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(os.getenv("EMAIL"), os.getenv("APP_PASSWORD"))
-        smtp.send_message(msg)
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.login("your_email@gmail.com", "your_app_password")
+        server.send_message(msg)
+        server.quit()
+
+        return True
+
+    except Exception as e:
+        print("Mail error:", e)
+        return False
