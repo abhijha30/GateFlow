e82959c6a5fa06ef28403144e9fce9e5877d0b40
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-04-03 17:22:49 +0530

    Update qr.py

--- utils/qr.py
+++ utils/qr.py
@@ -1,6 +1,14 @@
 import qrcode
+import os
 
-def generate_qr(data, path):
+def generate_qr(data, path="qr_codes"):
+    # 📁 create folder if not exists
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    file_path = f"{path}/{data}.png"
+
     img = qrcode.make(data)
-    img.save(path)
-    return path
+    img.save(file_path)
+
+    return file_path
